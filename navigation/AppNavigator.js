c1a5e8902ae0ddc17ff8fe6df295e980121f900a
Diego Oliveira <65927526+DiegoOliveira1990@users.noreply.github.com>
2024-09-23 14:11:33 -0300

    Ajustes de interface e atualização para versão 2.0

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { StatusBar, SafeAreaView, StyleSheet } from 'react-native';
 import LoginScreen from '../screens/LoginScreen';
 import SignupScreen from '../screens/SignupScreen';
 import FeedScreen from '../screens/FeedScreen';
@@ -14,15 +15,25 @@ const Stack = createStackNavigator();
 export default function AppNavigator() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="Signup" component={SignupScreen} />
-        <Stack.Screen name="Feed" component={FeedScreen} />
-        <Stack.Screen name="Profile" component={ProfileScreen} />
-        <Stack.Screen name="EditProfile" component={EditProfileScreen} />
-        <Stack.Screen name="Post" component={PostScreen} />
-        <Stack.Screen name="SearchUser" component={SearchUserScreen} />
-      </Stack.Navigator>
+      <SafeAreaView style={styles.safeArea}>
+        <StatusBar barStyle="dark-content" backgroundColor="#ffffff" />
+        <Stack.Navigator initialRouteName="Login">
+          <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
+          <Stack.Screen name="Signup" component={SignupScreen} options={{ headerShown: false }} />
+          <Stack.Screen name="Feed" component={FeedScreen} options={{ headerShown: false }} />
+          <Stack.Screen name="Profile" component={ProfileScreen} options={{ headerShown: false }} />
+          <Stack.Screen name="EditProfile" component={EditProfileScreen} options={{ headerShown: false }} />
+          <Stack.Screen name="Post" component={PostScreen} options={{ headerShown: false }} />
+          <Stack.Screen name="SearchUser" component={SearchUserScreen} options={{ headerShown: false }} />
+        </Stack.Navigator>
+      </SafeAreaView>
     </NavigationContainer>
   );
 }
+
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    backgroundColor: '#ffffff', // Defina a cor de fundo desejada para o SafeAreaView
+  },
+});
